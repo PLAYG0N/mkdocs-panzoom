2d12a0cc8f329bd55183a76abb30feebfecce0c8
PLAYG0N <playg0nofficial@gmail.com>
2024-08-24 22:53:26 +0200

    added more buttons

--- local/mkdocs-test-gh-pages/assets/javascripts/zoompan.js
+++ local/mkdocs-test-gh-pages/assets/javascripts/zoompan.js
@@ -1,12 +1,33 @@
+function add_buttons(box, instance) {
+  let max = box.querySelector(".panzoom-max");
+  max.addEventListener("click", function (e) {
+    instance.zoomTo(0, 0, 1);
+    console.log("clicked");
+  });
+  console.log(max);
+}
+
 function activate_zoom_pan() {
-  boxes = document.querySelectorAll(".mermaid");
+  boxes = document.querySelectorAll(".panzoom-box");
   console.log(boxes);
-  boxes.forEach((elem) => {
+  boxes.forEach((box) => {
+    elem = box.querySelector(".mermaid");
     //console.log(elem.nodeName);
     if (elem.nodeName == "DIV" && !elem.dataset.zoom) {
       console.log("Added");
       elem.dataset.zoom = true;
-      panzoom(elem);
+      let instance = panzoom(elem, {
+        minZoom: 1,
+        beforeWheel: function (e) {
+          var shouldIgnore = !e.altKey;
+          return shouldIgnore;
+        },
+        beforeMouseDown: function (e) {
+          var shouldIgnore = !e.altKey;
+          return shouldIgnore;
+        },
+      });
+      add_buttons(box, instance);
     }
     //console.log(elem);
   });
