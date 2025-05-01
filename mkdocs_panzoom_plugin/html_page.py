b0f56b5c8f019b2d8d815429f9fc58c9b261f391
PLAYG0N <playg0nofficial@gmail.com>
2025-05-01 14:48:11 +0200

    remove unused code

--- mkdocs_panzoom_plugin/html_page.py
+++ mkdocs_panzoom_plugin/html_page.py
@@ -23,13 +23,7 @@ class HTMLPage:
     def add_panzoom(self):
         for idx, element in enumerate(self.containers):
             panzoom_box = create_panzoom_box(self.soup,self.config,idx)
-            #panzoom_container = self.soup.new_tag("div",**{"class": "panzoom-container"})
-            test = element.wrap(panzoom_box)
-
             panzoom_box.append(create_info_box(self.soup,self.config))
-
-            #test.wrap(panzoom_container)
-        #print(self.soup.prettify())
 
         # Include the css and js in the file
         self.soup.head.append(create_css_link(self.soup,self.page))
